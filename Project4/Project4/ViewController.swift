067e27647b65c0bfc9513764de08f9bc082b1f30
Jamaal <jsedayao@gmail.com>
2016-05-11 13:12:45 -0400

    added stuff

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -25,13 +25,23 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = NSURL(string:"https://hackingwithswift.com")!
         webView.loadRequest(NSURLRequest(URL:url))
         webView.allowsBackForwardNavigationGestures = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: #selector(openTapped))
+    }
+    
+    func openTapped() {
+        let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "youtube.com", style: .Default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func openPage(action: UIAlertAction!) {
+        let url = NSURL(string: "https://" + action.title!)!
+        webView.loadRequest(NSURLRequest(URL: url))
     }
-
 
 }
 
